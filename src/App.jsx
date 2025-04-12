b5c2c92aefd37ea5f7938c409b3278e5aae8d8f3
Nick Pence <pencenj@gmail.com>
2025-04-12 06:09:40 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p style={{color: "white"}}>Still Not a Wiki</p>
+    </>
+  )
+}
+
+export default App
